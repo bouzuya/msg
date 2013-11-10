6de94d527da58c841e718bb710a77900d5d2f685
bouzuya <bouzuya@gmail.com>
2013-11-11 01:26:28 +0900

    added templates

---
+++ test/message.js
@@ -0,0 +1,14 @@
+var should = require('should');
+
+describe('message', function() {
+
+  describe('GET /messages', function() {
+  });
+
+  describe('GET /messages/:id', function() {
+  });
+
+  describe('POST /messages', function() {
+  });
+
+});
